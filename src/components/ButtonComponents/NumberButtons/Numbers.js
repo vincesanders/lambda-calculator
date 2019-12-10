638556aa01c317e005675296896738e663553669
Vincent Sanders <vincesanders@hotmail.com>
2019-12-10 16:11:58 -0600

    Completed MVP for the project.

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,6 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { numbers } from '../../../data';
+import NumberButton from './NumberButton';
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
@@ -7,13 +8,18 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+export default function Numbers() {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map((num, i) => {
+       return <NumberButton number={num} key={i} />
+       })}
     </div>
   );
 };
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,21 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { operators } from '../../../data'
+import OperatorButton from './OperatorButton';
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+export default function Operators() {
   // STEP 2 - add the imported data to state
+  const [operatorState, setOperatorState] = useState(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operators.map((op, i) => {
+       return <OperatorButton operator={op.char} key={i} />
+       })}
     </div>
   );
 };
